bb16ee477e7e62df7128b2b635ccb7aecafb6d4b
kcisneros <16183934+kcisneros@users.noreply.github.com>
2023-01-27 13:50:46 -0500

    Exercise 2 repeat string code

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,23 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+  // create array variable to hold an array of string objects
+  loopedArray = [];
+  // start first condition, if string is blank; return blank string
+  if (string === "") {
+    return "";
+    // else if num is less than 0; return "ERROR"
+  } else if (num < 0) {
+    return "ERROR"
+  }
+  // else condition
+  else {
+    // for loop. the body of the loop (pushing "hey" to the array) will happen so
+    // so long as i < num arg passed in by the test
+    for (let i = 0; i < num; i++) {
+        loopedArray.push("hey");
+    }
+    // returns a STRING, not array, with the commas taken out and replaced with nothing
+    return loopedArray.toString().replace(/,/g, '');
+  }
 
 };
 
