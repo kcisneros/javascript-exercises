335270de8edc89b0f3c14f0cf5ea917c5fc0520b
kcisneros <16183934+kcisneros@users.noreply.github.com>
2023-01-27 14:01:55 -0500

    Reverse string exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    // first take the parameter passed to the function and make it into an 
+    // array by calling `split` on it
+    let string_to_array = string.split("");
+    // create a variable to reverse the elements of the array (string_to_array var)
+    let reversed_array = string_to_array.reverse();
+    // create a reverse_string var which converts the array back to a string
+    // and replaces all commas with nothing.
+    let reverse_string = reversed_array.toString().replace(/,/g,  '');
+    // return the reverse string variable to the function
+    return reverse_string
 };
 
 // Do not edit below this line
