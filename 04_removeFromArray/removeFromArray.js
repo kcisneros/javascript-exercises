ebfeefce9ceb4ecadb51434d916340f72f731195
kcisneros <16183934+kcisneros@users.noreply.github.com>
2023-01-31 11:33:57 -0500

    Exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,22 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...numbers) {
+  // create new var which will hold the end result array
+  let new_array = [];
+  // start for loop for the array
+  for (i = 0; i < array.length; i++) {
+    // if the ith element in the array is included in the numbers array
+    if (numbers.includes(array[i]))
+    {
+      // continue (skip)
+      continue;
+    }
+    // else 
+    else {
+      // add the element from the array to the new_array
+      new_array.push(array[i]);
+    }
+  }
+  // return the new array to the function
+  return new_array;
 };
 
 // Do not edit below this line
