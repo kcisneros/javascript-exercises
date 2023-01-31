9b063270b3e47e6f4cd6785a70ad03b5bd03c3d5
kcisneros <16183934+kcisneros@users.noreply.github.com>
2023-01-31 15:02:54 -0500

    Exercise 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,18 @@
-const leapYears = function() {
+// leap year: year % 4 == 0; year % 400 == 0
+// not leap: year % 100 == 0
 
+const leapYears = function(year) {
+  // if the year is divisible by 100 AND it's not divisble by 400
+  // OR it's not divisible by 4 then it's not a leap year
+  if (year % 100 == 0 && year % 400 !== 0 || year % 4 !== 0) {
+//    console.log("not leap year");
+    return false;
+  }
+  // else it's a leap year
+  else {
+//    console.log("leap year");
+    return true;
+  }
 };
 
 // Do not edit below this line
