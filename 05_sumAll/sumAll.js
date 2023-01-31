6b11145f9ba3d1281b0b50414b4e623256c88ac7
kcisneros <16183934+kcisneros@users.noreply.github.com>
2023-01-31 17:48:34 -0500

    Exercise 5, revisited

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,46 +1,54 @@
-const sumAll = function(...numbers) {
-  let finalSum = 0;
-  // get the last item in array
-  // get the first item in array
-  // start with 0 counter
-  // if first item is less than or equal to last item
-  // increment 1
-  // add current item to last item
-  // when loop is done print last item
+// clunky code, not my favorite but it works.
+// this function takes two arguments as defined in the tests
+const sumAll = function(number1, number2) {
+  // set sum value to 0
   let currentSum = 0;
-  let sortedNumbers = numbers.sort();
 
-  // this code block works for the last test when tested individually
-  // but not as a part of the entire test suite
-  // if (typeof numbers !== 'number') {
-  //   console.log("ERROR");
-  //   return "ERROR";
-  // }
-
-  for (i = numbers[0]; i <= sortedNumbers.at(-1); i++) {
-    // if (typeof numbers[i] !== 'number') {
-    //   console.log("ERROR");
-    // }
-    // if (numbers.some(i => typeof i !== 'number')) {
-    //   console.log("ERROR");
-    //   return "ERROR";
-    // }
-    // if (isNaN(numbers[i])) {
-    //   console.log("ERROR");
-    //   return "ERROR";
-    // }
-    if (sortedNumbers[i] < 0 || numbers.some(i => typeof i === 'string')) {
-//      console.log("ERROR");
-      return "ERROR";
-    }
-    else {
-      currentSum += i;
-    }
+  // first check to see if the first param is greater than the second param.
+  // if it is, do the following
+  if (number1 > number2) {
+    // set a variable called lesserNum to number2 param passed in
+    lesserNumber = number2;
+    // greaterNumber is set to number1 param passed in
+    greaterNumber = number1;
   }
+  // else
+  else {
+    // lesserNumber is number1
+    lesserNumber = number1;
+    // greaterNumber is number2
+    greaterNumber = number2;
+  }
+
+  // next if block.. checks to make sure param is a number. if it's not error
+  if (typeof lesserNumber !== 'number' || typeof greaterNumber !== 'number') {
+    console.log("ERROR");
+    return("ERROR");
+  }
+
+  // for loop
+  for (i = lesserNumber; i <= greaterNumber; i++) {
+    // if the lesserNumber or the greaterNumber is less than 0; return error
+    if (lesserNumber < 0 || greaterNumber < 0) {
+      console.log("ERROR");
+      return ("ERROR");
+    }
+
+    // if the lesserNumber AND the greaterNumber are greater than 0
+    if (lesserNumber && greaterNumber > 0) {
+      // while loop. while lesserNumber is less than greaterNumber
+      while (i <= greaterNumber) {
+        // add lesserNumber (i) to currentSum
+        currentSum += i;
+        // increment lesserNumber (i) by 1 to move through range nums.
+        i++;
+      }
+    }
+    // return the currentSum to the function
   return currentSum;
+  }
 };
 
 // Do not edit below this line
 module.exports = sumAll;
 
-sumAll(10, [90, 1]);
