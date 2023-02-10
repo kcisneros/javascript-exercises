23c290b94635e907142e34fb274fde71d925b51b
kcisneros <16183934+kcisneros@users.noreply.github.com>
2023-02-10 16:36:46 -0500

    Exercise 8, Calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,41 @@
-const add = function() {
-	
+const add = function(firstNum, secondNum) {
+  return firstNum + secondNum;
 };
 
-const subtract = function() {
-	
+const subtract = function(firstNum, secondNum) {
+  return firstNum - secondNum;	
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+  let theSum = 0;
+  for (i = 0; i < numbers.length; i++) {
+    theSum += numbers[i];
+  }
+  return theSum;
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  // have to start at 1 because anything times 0 is 0
+  let theSum = 1;
+  for (i = 0; i < numbers.length; i++) {
+    theSum *= numbers[i];
+  }
+  return theSum;
 };
 
-const power = function() {
-	
+const power = function(numberOne, numberTwo) {
+  return Math.pow(numberOne, numberTwo);
 };
 
-const factorial = function() {
-	
+const factorial = function(numbers) {
+  let theSum = 1;
+  for (i = 1; i < numbers +1; i++) {
+    theSum *= i;
+  }
+  console.log(theSum);
+  return theSum;
 };
+
 
 // Do not edit below this line
 module.exports = {
