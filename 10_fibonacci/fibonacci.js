b0f773315fac9358e23b53615c04384464b9b078
kcisneros <16183934+kcisneros@users.noreply.github.com>
2023-02-13 15:42:42 -0500

    Fibonacci exercise - completed.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,33 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+
+  let firstNum = 0;
+  let secondNum = 1;
+
+  if (number <= 0) {
+    return "OOPS";
+  }
+
+  if (number <= 10) {
+    for (let i = 0; i < number; i++) {
+      firstNum = firstNum + secondNum;
+      // console.log(firstNum);
+      secondNum = firstNum + secondNum;
+      i++;
+      // console.log(secondNum);
+  
+    }
+    // console.log(firstNum);
+    return firstNum;
+  } else {
+    for (let i = 2; i < number; i++) {
+      firstNum = firstNum + secondNum;
+      // console.log(firstNum);
+      secondNum = firstNum + secondNum;
+      i++;
+    }
+    // console.log(secondNum);
+    return secondNum;
+  }
 
 };
 
