befcfabc262126ca881f1723584961ed70561953
kcisneros <16183934+kcisneros@users.noreply.github.com>
2023-02-13 10:03:09 -0500

    Palindrome exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,19 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  let stringWithoutPuncOrSpaces = string.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"").replace(/ +/g, "").toLowerCase();
+  let reverseAndJoinStringToArray = Array.from(string).reverse().join("");
+  let palindromeWithoutPuncOrSpaces = reverseAndJoinStringToArray.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"").replace(/ +/g, "").toLowerCase();
+  if (stringWithoutPuncOrSpaces === palindromeWithoutPuncOrSpaces) {
+    return true;
+  }
+  else {
+    return false;
+  }
 };
+
+
+//palindromes('racecar!');
+
+// palindromes('ZZZZ car, a man, a maracaz.');
 
 // Do not edit below this line
 module.exports = palindromes;
